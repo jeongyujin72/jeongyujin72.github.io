9b16ea995783522f58e6278bde82ebfc632b532f
jeongyujin72 <2022112010@dgu.ac.kr>
2024-11-15 05:31:29 +0900

    Create middle-point.js

---
+++ middle-point.js
@@ -0,0 +1,38 @@
+function calculateMeanCoordinates() {
+        var sumX = 0, sumY = 0;
+
+        coordinates.forEach(coord => {
+            sumX += coord.x;
+            sumY += coord.y;
+        });
+
+        var mean = {
+            x: sumX / coordinates.length,
+            y: sumY / coordinates.length
+        };
+        console.log("계산된 평균 좌표:", mean);
+      return mean;
+    }
+
+    var meanMarker = null; // 평균 마커를 저장할 변수
+
+    function addMeanMarker() {
+        if (coordinates.length === 0) return;
+
+        var meanCoord = calculateMeanCoordinates();
+        var meanLatLng = new daum.maps.LatLng(meanCoord.y, meanCoord.x);
+
+        // 기존 평균 마커가 있으면 제거
+        if (meanMarker) {
+            meanMarker.setMap(null);
+        }
+
+        // 새로운 평균 마커 추가
+        meanMarker = new daum.maps.Marker({
+            position: meanLatLng,
+            map: map,
+            title: "평균 위치"
+        });
+      map.setCenter(meanLatLng);
+      console.log("평균 마커가 추가되었습니다:", meanCoord);
+    }
