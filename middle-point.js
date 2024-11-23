d1f05f00e48dfeee105fa4635bb170dfa7a0605f
jeongyujin72 <2022112010@dgu.ac.kr>
2024-11-23 19:20:05 +0900

    Update middle-point.js

--- middle-point.js
+++ middle-point.js
@@ -45,26 +45,28 @@ function addMeanMarker() {
             map: map,
             title: "평균 위치"
         });
-map.setCenter(meanLatLng);
-console.log("평균 마커가 추가되었습니다:", meanCoord);
+        map.setCenter(meanLatLng);
+        console.log("평균 마커가 추가되었습니다:", meanCoord);
         
-// 지도에 표시할 원을 생성합니다
-var circle = new kakao.maps.Circle({
-      center : new kakao.maps.LatLng(meanCoord.y, meanCoord.x),  // 원의 중심좌표
-      radius: 1500, // 미터 단위의 원의 반지름입니다 
-      strokeWeight: 5, // 선의 두께입니다 
-      strokeColor: '#75B8FA', // 선의 색깔입니다
-      strokeOpacity: 1, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
-      strokeStyle: 'dashed', // 선의 스타일 입니다
-      fillColor: '#CFE7FF', // 채우기 색깔입니다
-      fillOpacity: 0.7  // 채우기 불투명도 입니다   
-    }); 
-        
-    // 지도에 원을 표시합니다 
-    circle.setMap(map);
-    console.log("원이 생성되었습니다");
+        // 지도에 표시할 원을 생성합니다
+        var circle = new kakao.maps.Circle({
+        center : new kakao.maps.LatLng(meanCoord.y, meanCoord.x),  // 원의 중심좌표
+        radius: 1500, // 미터 단위의 원의 반지름입니다 
+        strokeWeight: 5, // 선의 두께입니다 
+        strokeColor: '#75B8FA', // 선의 색깔입니다
+        strokeOpacity: 1, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+        strokeStyle: 'dashed', // 선의 스타일 입니다
+        fillColor: '#CFE7FF', // 채우기 색깔입니다
+        fillOpacity: 0.7  // 채우기 불투명도 입니다   
+        }); 
+            
+        // 지도에 원을 표시합니다 
+        circle.setMap(map);
+        console.log("원이 생성되었습니다");
+
+        // 원의 반경을 저장할 변수
+        var radius = circle.getRadius();
     }
 
-// 원의 반경을 저장할 변수
-var radius = circle.getRadius();
+
 export { meanMarker, circle, radius }
