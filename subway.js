48b16046bf7d0a8d4210b59994cb7f8d37413365
jeongyujin72 <2022112010@dgu.ac.kr>
2024-11-23 20:41:14 +0900

    Update subway.js

--- subway.js
+++ subway.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
             
             // 카테고리로 지하철역을 검색
             ps.categorySearch('SW8', placesSearchCB, {
-                location: meanMarker.getPosition()
+                location: meanMarker.getPosition(),
                 radius: radius
            });
         });
