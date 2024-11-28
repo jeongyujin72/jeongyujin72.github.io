810795b49174f940c7309cf657962396a45f37de
jeongyujin72 <2022112010@dgu.ac.kr>
2024-11-29 01:20:21 +0900

    Update subway.js

--- subway.js
+++ subway.js
@@ -11,6 +11,7 @@ var ps = new kakao.maps.services.Places(map);
 // console.log("circleRadius에 원의 반경 저장됨", circleRadius);
 
 
+
 document.addEventListener("DOMContentLoaded", function () {
 const completeButton = document.getElementById("complete-button");
 
@@ -21,24 +22,36 @@ const completeButton = document.getElementById("complete-button");
             // 원 내부의 마커 개수
             var markerCount = 0;
 
-            // 원 내부의 지하철역 검색
+            // 원 내부의 지하철 역 검색
             ps.categorySearch('SW8', placesSearchCB, {
                 location: meanMarker.getPosition(),
                 radius: radius
             });
+
             // 키워드 검색 완료 시 호출되는 콜백함수 입니다
             function placesSearchCB (data, status, pagination) {
                 if (status === kakao.maps.services.Status.OK) {
                     for (var i=0; i<data.length; i++) {
                         displayMarker(data[i]);    
                         console.log("data 배열에 저장된 장소가 마커로 표시되었음.", data[i]);
-                        markerCount = markerCount + 1;
-                        console.log("마커 개수 업데이트됨", markerCount);
-                    }       
+                    }
+                    markerCount = data.length;
+                    console.log("마커 개수 업데이트됨: ", markerCount);       
+                }
+                // 마커가 없으면 반경을 증가시키고 다시 검색
+                if (markerCount === 0) {
+                radius += 1000; // 반경 1000m씩 증가
+                console.log("마커가 없어 반경을 증가합니다. 새로운 반경:", radius);
+                
+                // 재귀 호출
+                ps.categorySearch('SW8', placesSearchCB, {
+                    location: meanMarker.getPosition(),
+                    radius: radius
+                }); 
                 }
             }
-    });
-}
+        });
+    }
 });
 
 
@@ -57,3 +70,4 @@ function displayMarker(place) {
         infowindow.open(map, marker);
     });
 }
+
